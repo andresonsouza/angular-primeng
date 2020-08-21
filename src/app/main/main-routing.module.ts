d7acd47859f4dc8f4e870160e5e9e9f35c9b93e9
Andreson Souza <andresonjazz@gmail.com>
2020-08-21 00:52:34 -0300

    feat: configure as rotas no módulo main

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,10 +2,17 @@ import { ProductsComponent } from './products/products.component';
 import { PeopleComponent } from './people/people.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'people', component: PeopleComponent},
-  { path: 'products', component: ProductsComponent}
+  {
+    path: '', component: HomeComponent,
+    children: [
+      { path: 'people', component: PeopleComponent },
+      { path: 'products', component: ProductsComponent }
+    ]
+  },
+
 ];
 
 @NgModule({
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { PeopleComponent } from './people/people.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent, PeopleComponent],
+  declarations: [ProductsComponent, PeopleComponent, HomeComponent],
   imports: [
     CommonModule,
     MainRoutingModule
