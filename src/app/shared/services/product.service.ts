bbaaea13f15f0604f051c32524b7dc2592eee2b8
Andreson Souza <andresonjazz@gmail.com>
2020-08-23 00:04:16 -0300

    feat: adicione os hosts nos arquivos de  environments

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -2,13 +2,14 @@ import { Product } from './../models/product';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  readonly url = 'http://localhost:3000';
+  readonly url = environment.host;
 
   constructor(private http: HttpClient) { }
 
